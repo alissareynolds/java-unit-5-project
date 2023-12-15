40f38fe1f642645eba62d1ed697421fda7ec7d8b
Alissa Reynolds <alissa6710@gmail.com>
2023-12-15 12:15:10 -0600

    added recursion example

--- src/main/java/org/example/Recursion.java
+++ src/main/java/org/example/Recursion.java
@@ -1,5 +1,22 @@
 package org.example;
 
 public class Recursion {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        System.out.println(recursive(numbers, 13));
+    }
+
+    public static int recursiveIndex(int[] array, int num, int index) {
+        if (index == array.length) {
+            return -1;
+        } else if (num == array[index]) {
+            return index;
+        }
+        return recursiveIndex(array, num, index + 1);
+    }
+
+    public static int recursive(int[] array, int num) {
+        return recursiveIndex(array, num, 0);
+    }
 
 }
