c82038f71e4fee964ccc794c1b34c7fdbbf6c995
Alissa Reynolds <alissa6710@gmail.com>
2023-12-15 11:21:14 -0600

    added linked list addition problem

--- src/main/java/org/example/LinkedListAddition.java
+++ src/main/java/org/example/LinkedListAddition.java
@@ -1,4 +1,51 @@
 package org.example;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class LinkedListAddition {
+    public static void main(String[] args) {
+        LinkedList one = new LinkedList();
+        LinkedList two = new LinkedList();
+        one.add(1);
+        one.add(2);
+        one.add(3);
+        two.add(4);
+        two.add(5);
+        two.add(6);
+
+        System.out.println("Adding Linked Lists Result: " + addingLinkedLists(one, two));
+
+    }
+
+    public static LinkedList addingLinkedLists(LinkedList firstList, LinkedList secondList) {
+
+        Iterator<Integer> first = firstList.descendingIterator();
+        Iterator<Integer> second = secondList.descendingIterator();
+
+        int unit = 1;
+        int firstLinkedListAsInt = 0;
+
+        while (first.hasNext()) {
+            firstLinkedListAsInt += unit * first.next();
+            unit *= 10;
+        }
+
+        unit = 1;
+        int secondLinkedListAsInt = 0;
+
+        while (second.hasNext()) {
+            secondLinkedListAsInt += unit * second.next();
+            unit *= 10;
+        }
+
+        int sum = firstLinkedListAsInt + secondLinkedListAsInt;
+        String str = String.valueOf(sum);
+
+        LinkedList<Integer> linkedListSum = new LinkedList<Integer>();
+        for (int i = 0; i < str.length(); i+=1) {
+            linkedListSum.add(Character.getNumericValue(str.charAt(i)));
+        }
+        return linkedListSum;
+    }
 }
--- src/main/java/org/example/Sorting.java
+++ src/main/java/org/example/Sorting.java
@@ -14,24 +14,24 @@ public class Sorting {
         merge(input, start, mid, end);
     }
 
-    public static void merge(int[] input, int start, int mid, int end) {
+    public static void merge(int[] input, int start, int middle, int end) {
 
-        if (input[mid - 1] <= input[mid]) {
+        if (input[middle - 1] <= input[middle]) {
             return;
         }
 
         int i = start;
-        int j = mid;
+        int j = middle;
         int tempIndex = 0;
-
         int[] temp = new int[end - start];
-        while (i < mid && j < end) {
+        while (i < middle && j < end) {
             temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
         }
 
-        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(input, i, input, start + tempIndex, middle - i);
         System.arraycopy(temp, 0, input, start, tempIndex);
 
     }
+}
 
 
